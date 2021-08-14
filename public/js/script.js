297b8ac732074ec13c3fe4176add970779d30e28
adityaharsh2001 <geekyadi118@gmail.com>
2021-08-14 07:15:29 +0000

    save

---
+++ public/js/script.js
@@ -0,0 +1,56 @@
+var brush = Math.floor(Math.random() * 9) + 1;
+for (var i = 0; i < brush; i++) {
+  var node = document.createElement("div");
+  var att = document.createAttribute("class");
+  att.value = "child";
+  node.setAttributeNode(att);
+  node.id = "child" + eval(i + 1);
+  node.innerHTML = '<img class="brush" src="./brush.png" alt=""></img>';
+  document.getElementsByClassName("parent")[0].appendChild(node);
+}
+
+var count = brush;
+$(function () {
+  console.log(count);
+  $(".draggable").draggable();
+  $("#droppable").droppable({
+    drop: function (event, ui) {
+      if (
+        $(ui.draggable).hasClass("draggable") &&
+        !$(ui.draggable).hasClass("dragged")
+      ) {
+        count++;
+        console.log(count);
+        $(".count").text(count);
+        $(ui.draggable).addClass("dragged");
+      }
+    },
+  });
+});
+
+var btn = document.getElementById("myBtn");
+if (count === 10) {
+  var modal = document.getElementById("correct");
+  var span = document.getElementsByClassName("close")[0];
+}
+
+
+else {
+  var modal = document.getElementById("wrong");
+  var span = document.getElementsByClassName("close")[1];
+}
+btn.onclick = function () {
+  modal.style.display = "block";
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var brush = Math.floor(Math.random() * 9) + 1
-for (var i = 0; i<brush; i++) {
+var brush = Math.floor(Math.random() * 9) + 1;
+for (var i = 0; i < brush; i++) {
   var node = document.createElement("div");
   var att = document.createAttribute("class");
   att.value = "child";
@@ -9,22 +9,48 @@ for (var i = 0; i<brush; i++) {
   document.getElementsByClassName("parent")[0].appendChild(node);
 }
 
-$(function() {
-  var count = brush;
+var count = brush;
+$(function () {
   console.log(count);
   $(".draggable").draggable();
   $("#droppable").droppable({
-    drop: function(event, ui) {
-      if ($(ui.draggable).hasClass("draggable") && (!$(ui.draggable).hasClass("dragged"))) {
+    drop: function (event, ui) {
+      if (
+        $(ui.draggable).hasClass("draggable") &&
+        !$(ui.draggable).hasClass("dragged")
+      ) {
         count++;
         console.log(count);
         $(".count").text(count);
         $(ui.draggable).addClass("dragged");
-       
-        if (count == 10) {
-          $(".count").text("Success !");
-        }
       }
-    }
+    },
   });
 });
+
+var btn = document.getElementById("myBtn");
+if (count === 10) {
+  var modal = document.getElementById("correct");
+  var span = document.getElementsByClassName("close")[0];
+}
+
+
+else {
+  var modal = document.getElementById("wrong");
+  var span = document.getElementsByClassName("close")[1];
+}
+btn.onclick = function () {
+  modal.style.display = "block";
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
