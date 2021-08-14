afc9889e625af811de036731025d1836b00a4127
adityaharsh2001 <geekyadi118@gmail.com>
2021-08-14 08:50:31 +0000

    save

--- script.js
+++ script.js
@@ -8,49 +8,48 @@ for (var i = 0; i < brush; i++) {
   node.innerHTML = '<img class="brush" src="./brush.png" alt=""></img>';
   document.getElementsByClassName("parent")[0].appendChild(node);
 }
-
-var count = brush;
-$(function () {
-  console.log(count);
-  $(".draggable").draggable();
-  $("#droppable").droppable({
-    drop: function (event, ui) {
-      if (
-        $(ui.draggable).hasClass("draggable") &&
-        !$(ui.draggable).hasClass("dragged")
-      ) {
-        count++;
+const counter = () => {
+  var count = brush;
+  $(function () {
+    $(".draggable").draggable();
+    $("#droppable").droppable({
+      drop: function (event, ui) {
+        if (
+          $(ui.draggable).hasClass("draggable") &&
+          !$(ui.draggable).hasClass("dragged")
+        ) {
+          count++;
+          $(".count").text(count);
+          $(ui.draggable).addClass("dragged");
+        }
         console.log(count);
-        $(".count").text(count);
-        $(ui.draggable).addClass("dragged");
-      }
-    },
+        var btn = document.getElementById("myBtn");
+        if (count === 10) {
+          var modal = document.getElementById("correct");
+          var span = document.getElementsByClassName("close")[0];
+        } else {
+          var modal = document.getElementById("wrong");
+          var span = document.getElementsByClassName("close")[1];
+        }
+        btn.onclick = function () {
+          modal.style.display = "block";
+          span.onclick = function () {
+            modal.style.display = "none";
+          };
+        };
+
+        window.onclick = function (event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        };
+        span.onclick = function () {
+          modal.style.display = "none";
+        };
+      },
+    });
+
+    // console.log(count);
   });
-});
-
-var btn = document.getElementById("myBtn");
-if (count === 10) {
-  var modal = document.getElementById("correct");
-  var span = document.getElementsByClassName("close")[0];
-}
-
-
-else {
-  var modal = document.getElementById("wrong");
-  var span = document.getElementsByClassName("close")[1];
-}
-btn.onclick = function () {
-  modal.style.display = "block";
-  span.onclick = function () {
-    modal.style.display = "none";
-  };
 };
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-span.onclick = function () {
-  modal.style.display = "none";
-};
+counter();
